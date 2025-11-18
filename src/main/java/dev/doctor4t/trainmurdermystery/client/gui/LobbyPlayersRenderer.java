e713eef837f705d99e3c0c5f9a45110a13d2720d
RAT <25477005+doctor4t@users.noreply.github.com>
2025-11-18 22:32:59 +0100

    Update LobbyPlayersRenderer.java

--- src/main/java/dev/doctor4t/trainmurdermystery/client/gui/LobbyPlayersRenderer.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/client/gui/LobbyPlayersRenderer.java
@@ -46,9 +46,9 @@ public class LobbyPlayersRenderer {
             context.getMatrices().translate(0, context.getScaledWindowHeight(), 0);
             context.getMatrices().scale(scale, scale, 1f);
             int i = 0;
-            for (Text text : TextUtils.getWithLineBreaks(Text.translatable("Thank you for playing The Last Voyage of the Harpy Express!\nMe and my team spent a lot of time working\non this mod and we hope you enjoy it.\nIf you do and wish to make a video or stream\nplease make sure to credit my channel, video and the mod page!\n - RAT / doctor4t"))) {
+            for (Text text : TextUtils.getWithLineBreaks(Text.translatable("Thank you for playing The Last Voyage of the Harpy Express!\nMe and my team spent a lot of time working\non this mod and we hope you enjoy it.\nIf you do and wish to make a video or stream\nplease make sure to credit my channel,\nvideo and the mod page!\n - RAT / doctor4t"))) {
                 i++;
-                context.drawTextWithShadow(renderer, text, 10, -80 + 10 * i, 0xFFFFFFFF);
+                context.drawTextWithShadow(renderer, text, 10, -90 + 10 * i, 0xFFFFFFFF);
             }
             context.getMatrices().pop();
         }
